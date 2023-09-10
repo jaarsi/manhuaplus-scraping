e4ff3b79d23e2b9aacf0a53693193c0d3a52d465
jairo <jairo@server.lan>
2023-09-10 23:53:16 +0000

    wip

--- src/series_scraping/__main__.py
+++ src/series_scraping/__main__.py
@@ -4,7 +4,6 @@ from . import cli, logging, settings
 
 logging.setup_logging()
 logger = logging.get_logger("series-scraping")
-os.makedirs(settings.APP_DIR, exist_ok=True)
 
 
 if __name__ == "__main__":
--- src/series_scraping/settings.py
+++ src/series_scraping/settings.py
@@ -3,6 +3,4 @@ from pathlib import Path
 
 DISCORD_WH = os.getenv("DISCORD_WH", None)
 DISCORD_TOKEN = os.getenv("DISCORD_TOKEN", "")
-DEFAULT_APP_DIR = Path.home() / ".series-scraping"
-APP_DIR = os.getenv("SERIES_SCRAPING_DATA_DIR", DEFAULT_APP_DIR)
-DATABASE_FILE = os.path.join(APP_DIR, "database.json")
+DATABASE_FILE = os.getenv("DATABASE_FILE", "docker/database.json")
